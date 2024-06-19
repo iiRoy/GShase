4ac6a0bc802e17f6e305fa0ca0b70e000104b8b7
Rodrigo <rock.gule@gmail.com>
2024-06-19 00:03:10 -0600

    Inicio CSS

---
+++ inicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Topos: Administrador</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="index">
+<div class="inicio">
+    <!--Header-->
+    <div class="header">
+        <div>
+            <img src="images/usuario_default.png">
+        </div>
+    </div>
+    <!--Welcome Container-->
+    <br>
+    <div class="container">
+        <div class="content">
+            <h1>¡Te damos la bienvenida a la Difusión de Artículos e Investigaciones de Alto Impacto!</h1>
+            <div class="buttons">
+                <button>
+                    <div class="button-circle"></div>
+                    Iniciar sesión
+                </button>
+            </div>
+            <div class="buttons">
+                <button>
+                    <div class="button-circle"></div>
+                    ¡Regístrate!
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <img src="images/welcome_image.png" alt="Example Image">
+        </div>
+    </div>
+</div>
+</body>
+</html>
