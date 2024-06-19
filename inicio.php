30b9c1f8d2e02eda2541e21ee189d309a11bb932
Rodrigo <rock.gule@gmail.com>
2024-06-19 02:02:02 -0600

    Inicio

--- inicio.php
+++ inicio.php
@@ -10,12 +10,13 @@
     <!--Header-->
     <div class="header">
         <div>
+            <img src="images/logo.png">
             <img src="images/usuario_default.png">
         </div>
     </div>
     <!--Welcome Container-->
     <br>
-    <div class="container">
+    <div class="welcome-container">
         <div class="content">
             <h1>¡Te damos la bienvenida a la Difusión de Artículos e Investigaciones de Alto Impacto!</h1>
             <div class="buttons">
@@ -33,6 +34,14 @@
         </div>
         <div class="content">
             <img src="images/welcome_image.png" alt="Example Image">
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="buscador"> <h1>Buscador</h1>
+        <div class="barra_busqueda">
+            <img src="images/ask.png" alt="Example Image">
+            <input type="text" placeholder="¿En qué te puedo ayudar?">
         </div>
     </div>
 </div>
